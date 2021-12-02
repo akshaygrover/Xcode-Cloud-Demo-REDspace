792337406ccfe025f6b84dd409a34ee1f2f4103c
Akshay Grover <akshaygrover588@gmail.com>
2021-12-02 13:17:05 -0500

    Initial Commit

---
+++ Shared/Xcode_Cloud_Demo_REDspaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_Cloud_Demo_REDspaceApp.swift
+//  Shared
+//
+//  Created by Akshay Grover on 2021-12-02.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_Cloud_Demo_REDspaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
