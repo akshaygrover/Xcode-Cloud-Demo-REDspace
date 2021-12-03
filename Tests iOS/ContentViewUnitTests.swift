caf5e4af4b4e0547d0f61effb3f243e5892537c3
Akshay Grover <akshaygrover588@gmail.com>
2021-12-03 13:52:03 -0500

    Added Viewinspertor SPM for unit testing

---
+++ Tests iOS/ContentViewUnitTests.swift
@@ -0,0 +1,32 @@
+//
+//  ContentViewUnitTests.swift
+//  Tests iOS
+//
+//  Created by Akshay Grover on 2021-12-03.
+//
+
+import XCTest
+import ViewInspector
+@testable import Xcode_Cloud_Demo_REDspace
+
+//extension ContentView: Inspectable { }
+
+class ContentViewUnitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+//    func testContentView() throws {
+//          let sut = ContentView()
+//          let text = try sut.inspect().text().string()
+//          XCTAssertEqual(text, "Hello, world!")
+//
+//      }
+
+
+}
